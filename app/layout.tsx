e7d51c1dcc12e1b4fb40464be9fbfc26f5bf9d05
Joshua James <joshuagjames@gmail.com>
2023-10-23 09:46:39 +0300

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ToastProvider } from '@/components/providers/toaster-provider'
 import { ConfettiProvider } from '@/components/providers/confetti-provider'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
